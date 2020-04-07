824df3d196bca00c655d7957cf38f2eddec5c57c
codeallthethingz <howdoicontactwill@gmail.com>
2020-04-07 16:56:27 -0700

    failing test. moved auth to header

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,6 @@ module.exports = {
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    '/src/githubator.js',
     '/src/asanator.js'
   ],
 
